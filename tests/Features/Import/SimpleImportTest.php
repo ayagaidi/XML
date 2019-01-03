c38e23890197c45a01354ca4b55b48c6bf11ee4c
Amando <amando@acfbentveld.nl>
2019-01-03 16:21:26 +0100

    Add xml stubs for future import tests

---
+++ tests/Features/Import/SimpleImportTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ACFBentveld\XML\Tests\Features\Import;
+
+use ACFBentveld\XML\Tests\TestCase;
+use ACFBentveld\XML\XML;
+
+class SimpleImportTest extends TestCase
+{
+    public function test_import()
+    {
+        $this->assertFalse(false);
+    }
+}
