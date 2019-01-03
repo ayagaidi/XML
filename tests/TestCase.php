bf468136d65edc5714f64c8d74508ec8d34fc697
Amando <amando@acfbentveld.nl>
2019-01-03 14:06:15 +0100

    Setup Orchestra/Testbench

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,33 @@
 
 namespace ACFBentveld\XML\Tests;
 
-use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use ACFBentveld\XML\XMLFacade;
+use ACFBentveld\XML\XMLServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+use Illuminate\Support\Facades\Artisan;
 use Spatie\Snapshots\MatchesSnapshots;
 
-abstract class TestCase extends PHPUnitTestCase
+abstract class TestCase extends Orchestra
 {
     use MatchesSnapshots;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        Artisan::call('view:clear');
+    }
+
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            XMLServiceProvider::class,
+        ];
+    }
+    protected function getPackageAliases($app)
+    {
+        return [
+            'XML' => XMLFacade::class,
+        ];
+    }
 }
